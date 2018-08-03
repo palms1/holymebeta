65e5089aa366649bb16af929cce86f81103aa58b
Palmor Almeida <palmor14@gmail.com>
2018-08-03 10:25:21 -0300

    google cloud with env

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
 
+require("dotenv").config(); // Loads .env
+
 const app = express();
 
 //Importing API ROUTES
